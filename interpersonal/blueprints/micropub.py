4023b0d6951814d4b4e628459a0ec5586aa63f5c
Micah R Ledbetter <me@micahrl.com>
2021-11-12 20:17:53 -0600

    Remove vestigial cruft from micropub bp

--- interpersonal/blueprints/micropub.py
+++ interpersonal/blueprints/micropub.py
@@ -7,22 +7,18 @@ endpoint in advance, so there is some coupling.
 import json
 import re
 
-import requests
 from flask import (
     Blueprint,
     current_app,
-    g,
     jsonify,
     render_template,
     request,
-    session,
     url_for,
 )
 
 from interpersonal import util
 from interpersonal.blueprints.indieauth import (
     ALL_HTTP_METHODS,
-    COOKIE_INDIE_AUTHED,
     InvalidBearerTokenError,
     bearer_verify_token,
     indieauth_required,
@@ -32,48 +28,6 @@ from interpersonal.util import render_error, json_error
 
 
 bp = Blueprint("micropub", __name__, url_prefix="/micropub")
-
-
-# TODO: code duplication from indieauth blueprint, can I fix?
-@bp.before_app_request
-def load_logged_in_user():
-    """Determine whether user is logged in before any view function runs
-
-    It is safe to do this in the cookie, because we are using encrypted cookies.
-    """
-    g.indieauthed = session.get(COOKIE_INDIE_AUTHED) is not None
-
-
-# TODO: add tests
-def verify_indieauth_access_token_via_http(token):
-    """Verify an IndieAuth access token (bearer token)
-
-    <https://indieweb.org/token-endpoint#Verifying_an_Access_Token>
-
-    Note that per that link:
-
-    > The URL of the token server must be known to the micropub endpoint in advance. The bearer token does not contain any information about the server address.
-    > This means that the micropub endpoint dictates the token endpoint that the user links to on his homepage.
-
-    This couples our micropub implementation (this blueprint)
-    to our indieauth implementation (the indieauth blueprint).
-
-    Note that this coupling means we COULD verify the token internally.
-
-    > Token-endpoints like https://tokens.indieauth.com that aim to interoperate with different micropub endpoint implementations MUST support this standard mechanism for verifying the token. However, if the token and micropub endpoints are tightly coupled (i.e. you control both implementations and expect them only to talk to each other), this verification can be done internally.
-
-    However, for now I'm going to do it as if these were two separate implementations. I'll learn it better this way, and it'll be more useful as an example to other implementors.
-    """
-
-    token_endpoint = util.absolute_url_for(current_app, "indieauth.bearer")
-    # token_endpoint = url_for("indieauth.bearer")
-    current_app.logger.debug(f"Verifying access token via endpoint {token_endpoint}")
-    response = requests.get(
-        token_endpoint, headers={"Authorization": f"Bearer {token}"}
-    )
-    response.raise_for_status()
-    verified_token = json.loads(response.content.decode())
-    return verified_token
 
 
 @bp.route("/")
